e3ff55c2f501df2a4ab6cc501cb39b3b324d6ce7
luci-nieradka-a <nieradka.luciane@escola.pr.gov.br>
2026-04-24 12:51:12 +0000

    criando os primeiros container

--- lib/paginas/telas/aula01.dart
+++ lib/paginas/telas/aula01.dart
@@ -6,12 +6,38 @@ class Aula01 extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar:AppBar(
-        title: Text("01 - Container")
-        ),
-      body: Container(
-        child: Text("Container"),
+      appBar: AppBar(
+        title: Text("01 - Container"),
       ),
-    ); // Scaffold
+      // Center centraliza o conteúdo
+      body: Center(
+        // Column coloca vários elementos como filhos
+        // Organiza os elementos na vertical
+        child: Column(
+          children: [
+            // Container com largura e altura
+            Container(
+              width: double.infinity, // Retorna o valor total disponível
+              height: 100,
+              color: Colors.deepOrange,
+              child: Center(child: Text("Primeiro Container"),
+               )
+              ),
+              // Container com padding e margin
+              Container(
+                padding: EdgeInsets.all(16),
+                margin: EdgeInsets.all(16),
+                color: Colors.blueAccent,
+                child: const Center(
+                  child: Text(
+                    "Segundo Container",
+                    style: TextStyle(color: Colors.white10),
+                    ),
+                ),
+              )
+          ],
+        ),
+      )
+    );
   }
 }
